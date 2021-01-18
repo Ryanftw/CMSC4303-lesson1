fe3c8d023dcf2caaead2a9d64dd9c666c142d2f0
Ryan Lorenz <rlorenz@uco.edu>
2021-01-18 15:59:14 -0600

    Bottom Nav Bar

--- lib/screen/start_screen.dart
+++ lib/screen/start_screen.dart
@@ -1,14 +1,78 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  static const routeName = '/startScreen'; 
+  static const routeName = '/startScreen';
   @override
   Widget build(BuildContext context) {
-    return Scaffold (
-      appBar: AppBar(
-        title: Text('Start Menu'),
-      ),
-      body: Text('Start Screen Welcome'),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Start Menu'),
+          actions: [
+            IconButton(
+                icon: Icon(Icons.alarm),
+                onPressed: () {
+                  print('Alarm');
+                }),
+            IconButton(
+                icon: Icon(Icons.message_sharp),
+                onPressed: () {
+                  print('Message');
+                })
+          ],
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              DrawerHeader(
+                child: Text('Drawer Header'),
+              ),
+              ListTile(
+                leading: Icon(Icons.message),
+                title: Text('Message'),
+              ),
+              ListTile(
+                leading: Icon(Icons.account_circle),
+                title: Text('Profile'),
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text('Settings'),
+              ),
+            ],
+          ),
+        ),
+        body: Column(
+          children: [
+            RaisedButton(
+              onPressed: () {
+                print('Menu 1');
+              },
+              child: Text('Menu1'),
+            ),
+            RaisedButton(
+              onPressed: () {
+                print('Menu 2');
+              },
+              child: Text('Menu 2'),
+            ),
+            Text('Third Menu'),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print('Floating action button');
+          },
+          child: Icon(Icons.add),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.business), label: 'Business'),
+          ],
+          currentIndex: 0,
+          onTap: (index) {
+            print('Current index = $index');
+          },
+        ));
   }
 }
